c8e146c1fc4dc07025b5253d7ccf28b791024c47
Fran <fjloopez@gmail.com>
2017-04-07 12:32:49 -0300

    cree archivo menu.php

--- php/main.php
+++ php/main.php
@@ -5,9 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="../css/borrar.css">
+  <link rel="stylesheet" href="../css/styles.css">
 </head>
 
 <body>
@@ -15,13 +13,7 @@
 <div class="container-fluid"> <!-- Abre el container-fluid -->
   <div class="row content">
     <div class="col-lg-6 col-xs-12 sidenav">  <!--Abre div de Menú -->
-      <h4 class="menu">Menú</h4>
-      <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="#section1">Home</a></li>
-        <li><a href="#section2">Log in</a></li>
-        <li><a href="#section3">Register</a></li>
-        <li><a href="#section3">FAQs</a></li>
-      </ul><br>
+      <?php include("menu.php"); ?> 
     </div> <!-- Cierra div de Menú -->
 
     <div class="col-lg-6 col-xs-12"> <!-- Abre div al costado del menu -->
---
+++ php/menu.php
@@ -0,0 +1,7 @@
+<h4 class="menu">Menú</h4>
+<ul class="nav nav-pills nav-stacked">
+	<li class="active"><a href="#section1">Home</a></li>
+	<li><a href="#section2">Log in</a></li>
+	<li><a href="#section3">Register</a></li>
+	<li><a href="#section3">FAQs</a></li>
+</ul><br>
