5b01bfc26dad75e5907c2625eccb8c4b1be44122
Fran <fjloopez@gmail.com>
2017-04-10 15:28:10 -0300

    visual de menu hamburguesa para mobile

--- php/main.php
+++ php/main.php
@@ -19,13 +19,18 @@
           <?php include 'menu.php'; ?>
       </div><!-- cierra container menu -->
       
-      <div class="container_img_main"> <!-- abre container imagen -->
-          <article>
-            <section>
-              <img class="img_character" src="..\img\Personajes.png" alt="Main character">
-            </section>
-          </article>
-      </div><!-- cierra container imagen -->
+      <div class="container_play"> <!-- abre container play -->
+
+        <div class="container_img_main"> <!-- abre container imagen -->
+          <img class="img_character" src="..\img\Personajes.png" alt="Main character">
+
+          <div class="container_play_button"> <!-- abre container del boton jugar -->
+            <button type="button" class="play_button">Play!</button>  
+          </div> <!-- cierra container del boton jugar -->
+
+        </div> <!-- cierra container imagen -->
+        
+      </div><!-- cierra container play -->
       
       <div class="slider">
           <img class="img_slider1" src="..\img\howdy.jpg">
--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,15 @@
 <header>
 	<nav class="nav_menu">
+		<!-- mm = menu mobile -->
+		<div class="container_mm">
+			<img class="img_logo_mm" src="..\img\logoVA.png" alt="Logo del juego">
+		</div>
+		<!-- mh = menu hamburguesa -->
+		<div class="container_mh">
+			<div class="top_mh"></div>
+			<div class="mid_mh"></div>
+			<div class="bot_mh"></div>
+		</div>
 		<ul class="list_menu">
 			<li><a href="main.php"><h4>Home</h4></a></li>
 			<li><a href="log_in.php"><h4>Log In</h4></a></li>
