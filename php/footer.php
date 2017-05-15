bc761155916336f28d470c743c947d6d432b005b
Ruypo <ruypo00@gmail.com>
2017-05-14 21:32:54 -0300

    "Footer"
    Footer mejorado, correccion del sprint 1, desaparece a partir de query
    tablet.
    Linea 185 de css provisoria.

--- php/footer.php
+++ php/footer.php
@@ -1,3 +1,7 @@
 <footer class="container_footer">
-        <p>¿Bugs? ¿Ideas? <a href="#">Contactanos</a></p>
+	<div class="containerFooter">
+	    <div class="footerLeft"> <p>¿Bugs? ¿Ideas? <a href="#">Contactanos </a></p> </div>
+    	<div class="footerCenter"> <p> POLÍTICA DE PRIVACIDAD </p> </div>
+    	<div class="footerRight"> <p> TODOS LOS DERECHOS RESERVADOS </p> </div>
+	</div>	
 </footer>
