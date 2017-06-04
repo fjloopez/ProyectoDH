d72776f634024317ae4f1b9ffc46ddef7763de9a
Ruypo <ruypo00@gmail.com>
2017-06-04 20:38:59 -0300

    Agregado Contacto
    Agregue la sección de Contacto, con su respectivo CSS y Query

--- php/footer.php
+++ php/footer.php
@@ -1,6 +1,6 @@
 <footer class="container_footer">
 	<div class="containerFooter">
-	    <div class="footerLeft"> <p>¿Bugs? ¿Ideas? <a href="#">Contactanos </a></p> </div>
+	    <div class="footerLeft"> <p>¿Bugs? ¿Ideas? <a href="contacto.php">Contactanos </a></p> </div>
     	<div class="footerCenter"> <p> POLÍTICA DE PRIVACIDAD </p> </div>
     	<div class="footerRight"> <p> &reg TODOS LOS DERECHOS RESERVADOS </p> </div>
 	</div>	
