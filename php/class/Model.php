b1b88b2bd343044c01d67c3e84c4031fee208bbd
Ruypo <ruypo00@gmail.com>
2017-06-13 13:53:24 -0300

    Videos 4 y 5
    Ya estan todos los cambios, hay q chequear con Maxi una anotacion q dejé
    en la linea 47 de Model.php

--- php/class/Model.php
+++ php/class/Model.php
@@ -10,13 +10,62 @@ class Model {
 
 	public function __construct($data)
 	{
-		foreach ($dara as $key => $value) {
-			if (in_array($key, $this->fillable)) {
-				$this->$key => $value;
-			}
-		}
+		$this->toModel($data);
 	}
 
+	public static function find($id)
+	{
+		$sql = 'SELECT * FROM '.static::$table.' WHERE id = :id';
+		$stmt = DB::getConn()->prepare($sql);
+		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+		$stmt->execute();
 
+		$result =  $stmt->fetch(PDO::FETCH_ASSOC);
+
+		$class = get_called_class();
+
+		$user = new $class ('','','','','','','');
+		$user->toUser($result);
+		return $user;
+	}
+
+	private function toModel($data)
+	{
+		if(isset($data['id'])) $this->id = $data['id'] 
+			foreach ($data as $key => $value) {
+				if (in_array($key, $this->fillable)) {
+				$this->$key => $value;
+				}
+			} 
+	}
+
+	public function saveUser(){
+		$sql = ($this->id)?$this->update()?$this->insert();
+		$stmt = DB::getConn()->prepare($sql);
+		
+		foreach ($this->fillable as $column) {
+			$stmt->bindValue(":$column", $this->$column);			//chequear el "$column" en video 5 (min 4:30)
+		}
+
+		$stmt->execute();
+	}
+
+	private function insert()
+	{
+		$columns = implode(', ' , $this->fillable);
+		$placeholders = ':'.implode(', ' , $this->fillable);
+		return $sql = "INSERT INTO ".static::$table." ($columns) VALUES ($placeholders)";
+	}
+
+	private function update()
+	{
+		
+		$set = '';
+		foreach ($this->fillable as $column) {
+			$set .= "$column=:$column,";
+		}
+		$set = trim($set, ", ");
+		return "UPDATE ".static::$table." SET $set WHERE id = ".$this->id;
+	}
 
 }
--- php/class/User.php
+++ php/class/User.php
@@ -19,63 +19,7 @@ class User extends Model {
 	private $password;
 
 	public $fillable = ['name', 'surname', 'username', 'email', 'birthDate', 'gender', 'password'];
-
-	public static function find($id)
-	{
-		$sql = 'SELECT * FROM user WHERE id = :id';
-		$stmt = DB::getConn()->prepare($sql);
-		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-		$stmt->execute();
-
-		$result =  $stmt->fetch(PDO::FETCH_ASSOC);
-
-		$user = new user ('','','','','','','');
-		$user-toUser($result);
-		return $user;
-	}
-
-	private function toUser($data)
-	{
-		$this->id = $data['id'];
-		$this->name = $data['name'];
-		$this->surname = $data['surname'];
-		$this->username = $data['username'];
-		$this->email = $data['email'];
-		$this->birthDate = $data['birthDate'];
-		$this->gender = $data['gender'];
-		$this->password = $data['password'];
-	}
-
-// En el video, Maxi saca esta función (es decir setPassword) y deja la parte del construct sólo con el $password que le pasan por parámetro
-
-	public function setPassword($value){
-		return $this->password = password_hash($value, PASSWORD_DEFAULT);
-	}
-
-
-	public function saveUser(){
-		$sql = ($this->id)?$this->update()?$this->insert();
-		$stmt = DB::getConn()->prepare($sql);
-		$stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
-		$stmt->bindValue(':surname', $this->surname, PDO::PARAM_STR);
-		$stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
-		$stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
-		$stmt->bindValue(':birthDate', $this->birthDate, PDO::PARAM_STR);
-		$stmt->bindValue(':gender', $this->gender, PDO::PARAM_STR);
-		$stmt->bindValue(':password', $this->password, PDO::PARAM_STR);
-		$stmt->execute();
-	}
-
-	private function insert()
-	{
-		return $sql = 'INSERT INTO user (name, surname, username, email, birth_date, gender, password) VALUES (:name, :surname, :username, :email, :birthDate, :gender, :password)';
-	}
-
-	private function update()
-	{
-		return 'UPDATE user SET name=:name, surname=:surname, username=:username, email=:email, birthDate=:birthDate, gender=:gender, password=:password WHERE id = '.$this->id
-	}
-
+	public static $table = 'user';
 
 
 }
